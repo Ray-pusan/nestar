cefb39d604b9ac85a392b2044faf7a68cf213676
Ray-pusan <raygithomework@gmail.com>
2025-12-29 11:47:16 +0900

    feat: develop login business logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, InputType, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 import { InternalServerErrorException, UsePipes, ValidationPipe } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
@@ -25,12 +25,12 @@ export class MemberResolver {
 
     @Mutation(() => Member)
     @UsePipes(ValidationPipe)
-    public async login(@Args("input") input: LoginInput): Promise<string> {
+    public async login(@Args("input") input: LoginInput): Promise<Member> {
          try {
             console.log("Mutation: login");
             console.log("input", input);
 
-            return this.memberService.login();
+            return this.memberService.login(input);
        
         } catch (err) {
             console.log("Error, login:", err);
