9f5c2ea061e9631d05218f349a1d62333eb17116
Ray-pusan <raygithomework@gmail.com>
2025-12-30 21:15:14 +0900

    feat: develop logging via global interceptors

--- apps/nestar-api/src/libs/interceptor/Logging.inteceptor.ts
+++ apps/nestar-api/src/libs/interceptor/Logging.inteceptor.ts
@@ -1,18 +1,38 @@
 
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
+import { stringify } from 'querystring';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        console.log('Before...');
 
-        const now = Date.now();
-        return next
-            .handle()
-            .pipe(
-                tap(() => console.log(`After... ${Date.now() - now}ms`)),
+    private readonly logger: Logger = new Logger();
+
+    public intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        const recordTime = Date.now();
+        const responsetType = context.getType<GqlContextType>();
+
+        if (responsetType === "http") {
+            /* Develop if needed! */
+        } else if (responsetType === "graphql") {
+            /* (1) Print Request */
+            const gplContext = GqlExecutionContext.create(context);
+            this.logger.log(`${this.stringify(gplContext.getContext().req.body)}`, "REQUEST");
+
+            /* (2) Errors handing via GraphQL */
+            /* (3) No Errors, giving Response below */
+            return next.handle().pipe(
+                tap((context) => {
+                    const responseTime = Date.now() - recordTime
+                    this.logger.log(`${this.stringify(context)} - ${responseTime}ms \n\n`, "RESPONSE");
+                }),
             );
+        }
+    }
+
+    private stringify(context: ExecutionContext): string {
+        return JSON.stringify(context).slice(0, 75);
     }
 }
