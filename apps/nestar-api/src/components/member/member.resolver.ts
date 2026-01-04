90a0c08d4affaccdb254005022835acd502a82f8
Ray-pusan <raygithomework@gmail.com>
2026-01-04 17:48:14 +0900

    fix: modify updateMember GraphQL Api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -9,6 +9,7 @@ import { ObjectId } from 'mongoose';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
 @Resolver()
 export class MemberResolver {
@@ -26,13 +27,6 @@ export class MemberResolver {
         return this.memberService.login(input);
     }
 
-    // Authenticated
-    @UseGuards(AuthGuard)
-    @Mutation(() => String)
-    public async updateMember(@AuthMember("_id") memberId: ObjectId): Promise<string> {
-        console.log("Mutation: updateMember");
-        return this.memberService.updateMember();
-    }
 
     @UseGuards(AuthGuard)
     @Query(() => String)
@@ -53,6 +47,15 @@ export class MemberResolver {
         return `Salom ${memberNick}`;
     }
 
+    // Authenticated
+    @UseGuards(AuthGuard)
+    @Mutation(() => Member)
+    public async updateMember(@Args("input") input: MemberUpdate, @AuthMember("_id") memberId: ObjectId): Promise<Member> {
+        console.log("Mutation: updateMember");
+        delete input._id;
+        return this.memberService.updateMember(memberId, input);
+    }
+
     @Query(() => String)
     public async getMember(): Promise<string> {
         console.log("Query: getMember");
@@ -66,7 +69,7 @@ export class MemberResolver {
     @Mutation(() => String)
     public async getAllMembersByAdmin(): Promise<string> {
         console.log("Mutation: getAllMembersByAdmin");
-        
+
         return this.memberService.getAllMembersByAdmin();
     }
 
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,12 +1,13 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from "@nestjs/mongoose"
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { Member } from '../../libs/dto/member/member';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
 import { createToken } from 'graphql/language/lexer';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
 @Injectable()
 export class MemberService {
@@ -44,15 +45,26 @@ export class MemberService {
         return response;
     }
 
-    public async updateMember(): Promise<string> {
-        return "updateMember executed";
+    public async updateMember(memberId: ObjectId, input: MemberUpdate): Promise<Member> {
+        const result: Member = await this.memberModel
+            .findByIdAndUpdate(
+                {
+                    _id: memberId,
+                    memberStatus: MemberStatus.ACTIVE
+                },
+                input,
+                { new: true }).exec();
+        if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+        
+        result.accessToken = await this.authService.createToken(result);
+        return result;
     }
 
     public async getMember(): Promise<string> {
         return "getMember executed";
     }
 
-    
+
     /** ADMIN **/
 
 
