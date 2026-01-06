bc98047da4327b9fdcb9479173550ddb419bd284
Ray-pusan <raygithomework@gmail.com>
2026-01-06 22:22:22 +0900

    feat: develop createProperty GraphQL api

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,18 +1,18 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from "@nestjs/mongoose"
 import { Model, ObjectId } from 'mongoose';
-import { Member, Members } from '../../libs/dto/member/member';
 import { AgentsInquiry, LoginInput, MemberInput, MembersInquiry } from '../../libs/dto/member/member.input';
 import { MemberStatus, MemberType } from '../../libs/enums/member.enum';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
 import { createToken } from 'graphql/language/lexer';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
-import { T } from '../../libs/types/common';
+import { StatisticModifier, T } from '../../libs/types/common';
 import { ViewService } from '../view/view.service';
 import { View } from '../../libs/dto/view/view';
 import { ViewInput } from '../../libs/dto/view/view.input';
 import { ViewGroup } from '../../libs/enums/view.enum';
+import { Member, Members } from '../../libs/dto/member/member';
 
 @Injectable()
 export class MemberService {
@@ -147,5 +147,18 @@ export class MemberService {
         const result: Member = await this.memberModel.findByIdAndUpdate({ _id: input._id }, input, { new: true }).exec();
         if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
         return result;
+    }
+
+    public async memberStatsEditor(input: StatisticModifier): Promise<Member> {
+        const { _id, targetKey, modifier } = input;
+        return await this.memberModel
+            .findByIdAndUpdate(
+                _id,
+                {
+                    $inc: { [targetKey]: modifier }
+                },
+                { new: true }
+            )
+            .exec();
     }
 }
--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -5,6 +5,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import PropertySchema from '../../schemas/Property.model';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
+import { MemberService } from '../member/member.service';
+import { MemberModule } from '../member/member.module';
 
 @Module({
   imports: [
@@ -15,7 +17,8 @@ import { ViewModule } from '../view/view.module';
       },
     ]),
     AuthModule,
-    ViewModule
+    ViewModule,
+    MemberModule
   ],
   providers: [PropertyResolver, PropertyService]
 })
