5e80b5c784c8fc6f945aadf8619fa8767f0b69c7
Ray-pusan <raygithomework@gmail.com>
2026-01-12 11:26:51 +0900

    feat: develop getProperties GraphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,7 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
-import { Property } from '../../libs/dto/property/property';
-import { PropertyInput } from '../../libs/dto/property/property.input';
+import { Properties, Property } from '../../libs/dto/property/property';
+import { PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { UseGuards } from '@nestjs/common';
@@ -14,7 +14,7 @@ import { PropertyUpdate } from '../../libs/dto/property/property.update';
 
 @Resolver()
 export class PropertyResolver {
-    constructor(private readonly popertyService: PropertyService) { }
+    constructor(private readonly propertyService: PropertyService) { }
 
     @Roles(MemberType.AGENT)
     @UseGuards(RolesGuard)
@@ -22,7 +22,7 @@ export class PropertyResolver {
     public async createProperty(@Args("input") input: PropertyInput, @AuthMember("_id") memberId: ObjectId): Promise<Property> {
         console.log("Mutation: createProperty");
         input.memberId = memberId;
-        return await this.popertyService.createProperty(input);
+        return await this.propertyService.createProperty(input);
     }
 
     @UseGuards(WithoutGuard)
@@ -33,7 +33,7 @@ export class PropertyResolver {
     ): Promise<Property> {
         console.log("Query: getProperty");
         const propertyId = shapeIntoMongoObjectId(input);
-        return await this.popertyService.getProperty(memberId, propertyId);
+        return await this.propertyService.getProperty(memberId, propertyId);
 
     }
 
@@ -46,7 +46,18 @@ export class PropertyResolver {
     ): Promise<Property> {
         console.log("Mutation: updateProperty");
         input._id = shapeIntoMongoObjectId(input._id);
-        return await this.popertyService.updateProperty(memberId, input);
+        return await this.propertyService.updateProperty(memberId, input);
 
+    }
+
+    @UseGuards(WithoutGuard)
+    @Query((returns) => Properties)
+    public async getProperties(
+        @Args("input") input: PropertiesInquiry,
+        @AuthMember("_id") memberId: ObjectId,
+    ): Promise<Properties> {
+        console.log("Query: getProperties");
+        return await this.propertyService.getProperties(memberId, input);
+        
     }
 }
