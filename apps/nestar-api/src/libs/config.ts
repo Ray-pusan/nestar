a60658b426f8b419f49df4209a096f680ec49f85
Ray-pusan <raygithomework@gmail.com>
2026-01-14 14:02:30 +0900

    feat: develop admin related boardArticle GraphQl APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,17 +1,19 @@
-import { ObjectId } from "bson";
+import { ObjectId } from 'bson';
 
-export const availableAgentSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews", "memberRank"];
-export const availableMemberSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews"];
+export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank'];
+export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
 
-export const availableOptions = ["propertyBarter", "propertyRent"];
+export const availableOptions = ['propertyBarter', 'propertyRent'];
 export const availablePropertySorts = [
-    "createdAt",
-    "updatedAt",
-    "propertyLikes",
-    "propertyViews",
-    "propertyRank",
-    "propertyPrice",
+	'createdAt',
+	'updatedAt',
+	'propertyLikes',
+	'propertyViews',
+	'propertyRank',
+	'propertyPrice',
 ];
+
+export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
 
 /** IMAGE CONFIGURATION **/
 import { v4 as uuidv4 } from 'uuid';
@@ -19,19 +21,19 @@ import * as path from 'path';
 
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
 export const getSerialForImage = (filename: string) => {
-    const ext = path.parse(filename).ext;
-    return uuidv4() + ext;
+	const ext = path.parse(filename).ext;
+	return uuidv4() + ext;
 };
 
 export const shapeIntoMongoObjectId = (target: any) => {
-    return typeof target === "string" ? new ObjectId(target) : target;
-}
+	return typeof target === 'string' ? new ObjectId(target) : target;
+};
 
 export const lookupMember = {
-    $lookup: {
-        from: "members",
-        localField: "memberId",
-        foreignField: "_id",
-        as: "memberData",
-    },
+	$lookup: {
+		from: 'members',
+		localField: 'memberId',
+		foreignField: '_id',
+		as: 'memberData',
+	},
 };
